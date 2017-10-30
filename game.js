a1448be2e4c2ef6b9ca63885dc048156dc68d2ac
lacazeto <lucas.cazeto@gmail.com>
2017-10-30 23:10:30 +0100

    added innerhtml via js, separated js files

---
+++ game.js
@@ -0,0 +1,90 @@
+function Level() {
+    this.rows = 10;
+    this.columns = 10;
+    this.playerPos = [0, 5];
+    this.treasurePos = [9, Math.floor(Math.random() * (9 - 1) + 1)];
+    this.shark1Pos = [Math.floor(Math.random() * (4 - 1) + 1), 9];
+    this.shark2Pos = [Math.floor(Math.random() * (8 - 5) + 5), 9];
+    this.gridLine = [];
+    this.scale = 20;
+
+    //create a DOM element and assign its class
+    this.domElement = function (name, className) {
+        var element = document.createElement(name);
+        if (className) {
+            element.className = className;
+        }
+        return element;
+    };
+    //add empty elements to the grid
+    this.createBlankGrid = function (){
+        for(gridCount = 0; gridCount < this.columns; gridCount++){
+            this.gridLine[gridCount] = [];
+        }
+    };
+     //labeling level's grid cells to display actors locations
+     this.labelGrid = function () {
+        for (var x = 0; x < this.columns; x++) {
+            var fieldType = null;
+            for (var y = 0; y < this.rows; y++) {
+                if (x === 0 && y === 5) {
+                    this.gridLine[x].push("player");
+                }
+                else{
+                    this.gridLine[x].push(fieldType);
+                }
+            }
+        }
+        this.addShark(this.shark1Pos);
+        this.addShark(this.shark2Pos);
+        this.addTreasure(this.treasurePos);
+
+    };
+    this.addShark = function (shark) {
+        this.gridLine[shark[0]][shark[1]] = "shark";
+    };
+    this.addTreasure = function (treasure) {
+        this.gridLine[treasure[0]][treasure[1]] = "treasure";
+    };
+    //draw game's table (grid)
+    this.drawGrid = function () {
+        var table = this.domElement("table", "background");
+        table.style.width = 10 * this.scale + "px";
+        var row;
+        var element;
+        for(gridCount = 0; gridCount < (this.rows * this.columns); gridCount++){
+            if(gridCount % 10 === 0){
+                row = table.appendChild(this.domElement("tr"));
+                row.style.height = this.scale + "px";
+            }
+            element = row.appendChild(this.domElement("td"));
+        }
+
+        for (var x = 0; x < this.columns; x++) {
+            for (var y = 0; y < this.rows; y++) {
+                if(this.gridLine[x][y] !== null){
+                    var actor = element.appendChild(this.domElement("div", "actor " + this.gridLine[x][y]));
+                    actor.style.width = this.scale + "px";
+                    actor.style.height = this.scale + "px";
+                    actor.style.left = this.scale + "px";
+                    actor.style.top = this.scale + "px"; 
+                }
+            }
+        }
+
+        return table;
+    };
+    this.domDisplay = function (parent, level) {
+        var gridContainer = parent.appendChild(this.domElement("div", "game-grid"));
+        this.createBlankGrid();
+        this.labelGrid();
+        gridContainer.appendChild(level.drawGrid());
+    };
+}
+
+var newLevel = new Level();
+
+window.onload = function () {
+    var grid = document.getElementsByClassName("game");
+    newLevel.domDisplay(grid[0], newLevel);
+};
--- script.js
+++ script.js
@@ -1,77 +1,13 @@
-function Level() {
-    this.rows = 10;
-    this.columns = 10;
-    this.playerPos = [0, 5];
-    this.shark1Pos = [9, Math.floor(Math.random() * (4 - 1) + 1)];
-    this.shark2Pos = [9, Math.floor(Math.random() * (8 - 5) + 5)];
-    this.gridLine = [];
-    this.scale = 20;
-
-    //create DOM element and assign a class
-    this.domElement = function (name, className) {
-        var element = document.createElement(name);
-        if (className) {
-            element.className = className;
-        }
-        return element;
-    };
-     //labeling level's grid cells to display actors locations
-     this.labelGrid = function () {
-        for (var y = 0; y < this.columns; y++) {
-            var fieldType = null;
-            for (var x = 0; x < this.rows; x++) {
-                this.gridLine.push(fieldType);
-                if (x === 0 && y === 5) {
-                    this.gridLine.push("player");
-                }
-            }
-        }
-        this.addShark(this.shark1Pos);
-        this.addShark(this.shark2Pos);
-
-    };
-    this.addShark = function (shark) {
-        this.gridLine[shark[0] * shark[1]] = "shark";
-        return true;
-    };
-    //draw game's table (grid)
-    this.drawGrid = function () {
-        var table = this.domElement("table", "background");
-        table.style.width = 10 * this.scale + "px";
-        var row;
-        for(gridCount = 0; gridCount < this.gridLine.length; gridCount++){
-            if(gridCount % 10 === 0){
-                row = table.appendChild(this.domElement("tr"));
-                row.style.height = this.scale + "px";
-            }
-            row.appendChild(this.domElement("td"));
-
-            if(this.gridLine[gridCount] !== null){
-                this. 
-            }
-        }
-        return table;
-    };
-    this.domDisplay = function (parent, level) {
-        var gridContainer = parent.appendChild(this.domElement("div", "game-grid"));
-        this.labelGrid();
-        gridContainer.appendChild(level.drawGrid());
-        var actor = this.domElement("div");
-        for (item = 0; item < level.gridLine.length; item++) {
-            if (item !== null) {
-                var rect = gridContainer.appendChild(this.domElement("div", "actor " + level.gridLine[item]));
-                rect.style.width = this.scale + "px";
-                rect.style.height = this.scale + "px";
-                rect.style.left = this.scale + "px";
-                rect.style.top = this.scale + "px";
-            }
-        }
-    }
+function drawLandingPage(){
+    document.body.innerHTML = "<header><h1 class='title'>TREASURE HUNTER</h1></header><main><div class='central-container'><div class='img-container'><img class='img-splash' id='img-diver' src='images/owd-diver-min.png' alt='scuba-diver'></div><div class='img-container'><img class='img-splash' src='images/great-white-shark-min.png' alt='shark'></div></div></main><footer class='splash-footer'><button class='btn-play'><i class='fa fa-caret-square-o-right' aria-hidden='true'></i> Play</button></footer>";    
 }
 
-var newLevel = new Level();
+function drawGamingPage(){
+    
+}
 
 window.onload = function () {
-    var grid = document.getElementsByClassName("game");
-    newLevel.domDisplay(grid[0], newLevel);
+    drawLandingPage();
+    var button = document.getElementsByClassName("btn-play");
+    button.addEventListener("click", drawGamingPage);
 };
